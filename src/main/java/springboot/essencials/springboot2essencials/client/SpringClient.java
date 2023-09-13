1ab7e351190a3da46290e311a6c9aaf01c2dce8a
Gabriel Santana <95727078+gapesasi@users.noreply.github.com>
2023-09-13 07:43:44 -0300

    Utilizando métodos RestTemplate POST para trabalhar com requisição POST do controller. Aula atual: 29/51

--- src/main/java/springboot/essencials/springboot2essencials/client/SpringClient.java
+++ src/main/java/springboot/essencials/springboot2essencials/client/SpringClient.java
@@ -2,8 +2,7 @@ package springboot.essencials.springboot2essencials.client;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import springboot.essencials.springboot2essencials.domain.Anime;
 
@@ -25,10 +24,33 @@ public class SpringClient {
                 .getForObject("http://localhost:8080/animes/all", Anime[].class);
         log.info(Arrays.toString(animes));
 
-        ResponseEntity<List<Anime>> animesList = new RestTemplate()
+        ResponseEntity<List<Anime>> exchange = new RestTemplate()
                 .exchange("http://localhost:8080/animes/all", HttpMethod.GET,
-                        null, new ParameterizedTypeReference<>() {
-                        });
-        log.info(animesList.getBody());
+                        null,
+                        new ParameterizedTypeReference<>() {
+                        }
+                );
+        log.info(exchange.getBody());
+
+        //Aula 28 - RestTemplate POST
+//        Anime kingdom = Anime.builder().name("kigndom").build();
+//        Anime kingdomSaved = new RestTemplate()
+//                .postForObject("http://localhost:8080/animes", kingdom, Anime.class);
+//        log.info("Saved anime {}", kingdomSaved);
+
+        Anime samuraiChamploo = Anime.builder().name("Samurai Champloo").build();
+        ResponseEntity<Anime> samuraiChamplooSaved = new RestTemplate()
+                .exchange("http://localhost:8080/animes",
+                        HttpMethod.POST,
+                        //Método Post requer o HttpEntity
+                        new HttpEntity<>(samuraiChamploo, createJsonHeader()),
+                        Anime.class);
+        log.info("Saved anime {}", samuraiChamplooSaved);
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
